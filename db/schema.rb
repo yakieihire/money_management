937e743d1929b7831c0f667763093ed8af9f15c5
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-05-11 20:12:59 +0900

    モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_06_130246) do
+ActiveRecord::Schema.define(version: 2023_05_10_080028) do
+
+  create_table "incomes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "income_amount", null: false
+    t.date "register_day", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_incomes_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2023_05_06_130246) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "incomes", "users"
 end
