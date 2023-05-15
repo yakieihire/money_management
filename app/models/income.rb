279afd525627ed65b8411150797ee24941ac8989
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-05-15 19:40:08 +0900

    ActiveHashの導入

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,2 +1,7 @@
 class Income < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :user
+
+  validates :category_id, numericality: { other_than: 1 } 
 end
