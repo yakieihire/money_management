170b7430a89831dc73f05f3a152f4313eaf6e338
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-05-15 22:07:53 +0900

    バリデーション記述

--- app/models/income.rb
+++ app/models/income.rb
@@ -4,4 +4,6 @@ class Income < ApplicationRecord
   belongs_to :user
 
   validates :category_id, numericality: { other_than: 1 } 
+  validates :income_amount, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 9_999_999 }
 end
