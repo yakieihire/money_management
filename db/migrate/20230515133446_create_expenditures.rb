7cdf0df65ffb80194a81f1d6c4123b4db9cf5e28
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-05-15 22:37:48 +0900

    モデル作成

---
+++ db/migrate/20230515133446_create_expenditures.rb
@@ -0,0 +1,12 @@
+class CreateExpenditures < ActiveRecord::Migration[6.0]
+  def change
+    create_table :expenditures do |t|
+      t.integer :category2_id, null: false
+      t.integer :expenditure_amount, null: false
+      t.date :register_day, null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :income, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
