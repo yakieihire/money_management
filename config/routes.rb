67ddb8a21915669daf358356bfb76467f8c6bcdc
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-05-19 21:29:49 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :incomes, only: [:new ,:create]
-  resources :expenses, only: [:new ,:create]
+  resources :expenditures, only: [:new ,:create]
   resources :diffs, only: [:index]
 
   root 'transactions#index'
