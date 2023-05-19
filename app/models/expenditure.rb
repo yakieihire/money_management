6176238bf6a82fe86b3fe052dcfb286766d2e5d8
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-05-19 21:28:31 +0900

    バリデーション変更

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,2 +1,9 @@
 class Expenditure < ApplicationRecord
+  belongs_to :selection
+  belongs_to :user
+  belongs_to :income
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :expenditure_amount, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 9_999_999 }
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -2,8 +2,8 @@ class Income < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :user
+  has_many :expenditures
 
-  validates :category_id, numericality: { other_than: 1 } 
   validates :income_amount, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 9_999_999 }
 end
